351c9bf1c4a656c42b287c34c8c7d86b2f3c8996
Adam Spruijt <adamspruijt@gmail.com>
2013-06-24 14:37:57 -0400

    Better handling of field descriptions

--- LanguageFieldTabs.js
+++ LanguageFieldTabs.js
@@ -3,34 +3,41 @@ $(function(){
 	var $langField = $(".Inputfield > .ui-widget-content").has(".LanguageSupport");
 	$langField.each(function(){
 		var $this = $(this);
+		console.log($this.children(".LanguageSupport").length);
 		var $langBox = $this.children(".LanguageSupport");
+		if ( $langBox.length > 1) { //should avoid applying to single language field like Languge "alt" fields
 
-		var fieldID = $this.parent(".Inputfield").attr("id");
+			var fieldID = $this.parent(".Inputfield").attr("id");
 
-		// markup variables
-		var LangTabsBox = "<div class='langTabs'><ul></ul></div>";
-		var i = 0;
+			// markup variables
+			var LangTabsBox = "<div class='langTabs'><ul></ul></div>";
+			var i = 0;
 
-		// add the markup the LangTabs will go inside
-		$this.prepend(LangTabsBox);
-		var $langTabsBox = $this.children(".langTabs"); 
-		var $langTabs = $langTabsBox.children("ul"); 
-
-
-		$langBox.each(function(){
-
-			var $this = $(this);
+			// add the markup the LangTabs will go inside
+			$this.append(LangTabsBox);
+			var $langTabsBox = $this.children(".langTabs"); 
+			var $langTabs = $langTabsBox.children("ul"); 
 
 
-			var label = $this.children("label").text();
-			var anchor = fieldID+i;
+			$langBox.each(function(){
 
-			$langTabs.append("<li><a href='#"+anchor+"'>"+label+"</a></li>");
-			$this.attr("id", anchor).appendTo($langTabsBox);
+				var $this = $(this);
 
-			i++;
-		});
-		$langTabsBox.tabs();
+
+				var label = $this.children("label").text();
+				var anchor = fieldID+i;
+
+				$langTabs.append("<li><a href='#"+anchor+"'>"+label+"</a></li>");
+				$this.attr("id", anchor).appendTo($langTabsBox);
+
+				i++;
+			});
+
+			// $langTabsBox.prepend($langTabsBox.siblings(".description"));
+
+			$langTabsBox.tabs();
+		}
+
 	});
 
 });
